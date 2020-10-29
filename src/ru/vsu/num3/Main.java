51bcdf7ca3e730a3ba834c8d429b652f52ac1127
Egor Kirin <egorcom27@mail.ru>
2020-10-29 17:54:32 +0300

    task6

--- src/ru/vsu/num3/Main.java
+++ src/ru/vsu/num3/Main.java
@@ -8,49 +8,60 @@ public class Main {
         double n = readDouble("n");
         double e = readDouble("e");
 
+        if (!checkInput(x)) {
+            System.out.println("X entered out of range (-1, 1)");
+            return;
+        }
+
+        double resultSumOfSequenceEpsilon = calculateSumOfSequenceEpsilon(x, e, 1);
+        double numberOfIterationsGreaterThanEpsilon = calculateSumOfSequenceEpsilon(x, e, 0);
+        double resultSumOfSequenceEpsilonDividedBy10 = calculateSumOfSequenceEpsilon(x, e / 10, 1);
+        double numberOfIterationsGreaterThanEpsilonDividedBy10 = calculateSumOfSequenceEpsilon(x, e / 10, 0);
         double resultSumOfSequence = calculateSumOfSequence(x, n);
-        printResult("of the first N elements", resultSumOfSequence);
-
-        double resultSumOfSequenceEpsilon = calculateSumOfSequenceEpsilon(x, n, e);
-        printResult("of the first N elements superior in absolute value of the epsilon number",
-                resultSumOfSequenceEpsilon);
-
-        double resultSumOfSequenceEpsilonDivisionBy10 = calculateSumOfSequenceEpsilon(x, n, e / 10);
-        printResult("of the first N elements superior in absolute value of the epsilon divided 10 number",
-                resultSumOfSequenceEpsilonDivisionBy10);
-
         double resultSumFunction = calculateFunction(x);
-        printResult("function using Math", resultSumFunction);
+
+        printResultSum("of the first N elements", resultSumOfSequence);
+
+        printResultSum("superior in absolute value of the epsilon number",
+                numberOfIterationsGreaterThanEpsilon, resultSumOfSequenceEpsilon);
+
+        printResultSum("superior in absolute value of the epsilon divided 10 number",
+                numberOfIterationsGreaterThanEpsilonDividedBy10, resultSumOfSequenceEpsilonDividedBy10);
+
+        printResultSum("function using Math", resultSumFunction);
+    }
+
+    private static boolean checkInput(double x) {
+        return x > -1 && x < 1;
     }
 
     private static double calculateSumOfSequence(double x, double n) {
-        double sum = 1;
+        double sequenceTerm = 1;
+        double sumN = 0;
 
-        for (int i = 0; i < n - 1; i++) {
-            double result = calculateNMemberOfSequence(i, x);
-            sum += result;
+        for (int i = 0; i < n; i++) {
+            sumN += sequenceTerm;
+            sequenceTerm *= -x;
         }
-        return sum;
+        return sumN;
     }
 
-    private static double calculateSumOfSequenceEpsilon(double x, double n, double e) {
+    private static double calculateSumOfSequenceEpsilon(double x, double e, int index) {
         double sumEpsilon = 0;
+        double sequenceTerm = 1;
+        int count = 0;
 
-        for (int i = 0; i < n - 1; i++) {
-            double result = calculateNMemberOfSequence(i, x);
-            if (Math.abs(result) > e) {
-                sumEpsilon += result;
-            }
+        while (Math.abs(sequenceTerm) > e) {
+            sumEpsilon += sequenceTerm;
+            sequenceTerm *= -x;
+            count++;
         }
-        return sumEpsilon;
+        double[] arraySum = {count, sumEpsilon};
+        return arraySum[index];
     }
 
     private static double calculateFunction(double x) {
         return 1 / (1 + x);
-    }
-
-    private static double calculateNMemberOfSequence(double i, double x) {
-        return Math.pow(-1, i + 1) * (x + Math.pow(x, i + 1));
     }
 
     private static double readDouble(String name) {
@@ -59,7 +70,11 @@ public class Main {
         return scanner.nextDouble();
     }
 
-    private static void printResult(String phrase, double resultSum) {
-        System.out.printf("The sum %s --> %2$.2f\n", phrase, resultSum);
+    private static void printResultSum(String phrase, double resultSum) {
+        System.out.printf("The sum %s --> %.4f\n", phrase, resultSum);
+    }
+
+    private static void printResultSum(String phrase, double resultCount, double resultSum) {
+        System.out.printf("The sum of the first %.0f elements %s --> %.4f\n", resultCount, phrase, resultSum);
     }
 }
